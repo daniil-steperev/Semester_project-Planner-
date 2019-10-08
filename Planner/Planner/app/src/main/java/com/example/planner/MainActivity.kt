1b7db66da9db1742d076cd5c4f1706c4bfc574c5
daniil.stepyrev <danik-steperev@mail.ru>
2019-10-08 22:21:56 +0300

    added Planner first version (does not work)

---
+++ Planner/Planner/app/src/main/java/com/example/planner/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.planner
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
